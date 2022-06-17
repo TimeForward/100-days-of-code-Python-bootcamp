aff5c4e113e110aaa827eed8848af87d5ae9b518
TimeForward <roman.nasyrov@gmail.com>
2022-06-17 21:41:32 +0300

    day 1 final project + theory

---
+++ day 1 theory.py
@@ -0,0 +1,13 @@
+# string concatenation +
+# new string characters
+# + input function
+#print many lines in one statement:
+print("Hello world\nHello world\nHello world")
+print("Hello"+" "+"Angela")
+
+# first input is prompted:
+#then print() will print "Hello" and the user input
+print("Hello " + input("What is your name?"))
+
+# added len()
+print(len(input("What is your name?")))
