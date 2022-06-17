c06cbfd359d440dbe47048972ef360336fe7b67f
TimeForward <roman.nasyrov@gmail.com>
2022-06-17 15:04:30 +0300

    day 1 exercise 2

---
+++ day 1-2.py
@@ -0,0 +1,9 @@
+# Day 1 - String Manipulation:
+# write code that outputs exactly the below text:
+# String Concatenation is done with the "+" sign.
+# e.g. print("Hello " + "world")
+# New lines can be created with a backslash and n.
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
