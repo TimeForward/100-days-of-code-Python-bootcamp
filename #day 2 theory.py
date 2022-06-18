7d72a46a2340227c3446e72b56542facbccc352f
TimeForward <roman.nasyrov@gmail.com>
2022-06-18 09:01:06 +0300

    day 2 theory + day 2 exercise 1

---
+++ #day 2 theory.py
@@ -0,0 +1,31 @@
+# #day 2
+# # data types
+# #String
+# #subtsring with index
+# # prints H
+# print("Hello"[0])
+# # prints O
+# print("Hello"[4])
+# #concatenation
+# print("123"+"345")
+# #Integer
+# #print sum
+# print(123 + 345)
+# #Python ignores underscores -they are for ease of visual pereceiving 
+# 123_456_789
+# #float
+# 3.14159
+# #Boolean
+# True
+# False
+
+# results in error : str + int
+num_char = len(input("What is your name?"))
+#print("Your name has " + num_char + "characters")
+# <class 'int'>
+# print(type(num_char))
+# # <class 'int'>
+# #type conversion/casting
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters")
+# Mathematical operations
---
+++ #day 2-1 Write a program that adds the digits in a 2 digit number.py
@@ -0,0 +1,21 @@
+#day 2-1 Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+#get the first and second digits using subscripting/index notation
+a =int(two_digit_number[0])
+b =int(two_digit_number[1])
+#print the sum
+print(a+b)
+
+
+
+
+
+
+
