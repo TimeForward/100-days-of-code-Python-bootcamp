3e711330d80fe106a6f9cf69c71627153a71c542
TimeForward <roman.nasyrov@gmail.com>
2022-06-24 20:48:41 +0300

    day 4 project completed

--- # day 4 project Rock,Sciccors, Paper.py
+++ # day 4 project Rock,Sciccors, Paper.py
@@ -29,9 +29,49 @@ scissors = '''
 import random
 test_seed = int(input("Create a seed number: "))
 random.seed(test_seed)
-user_choice=input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors.")
-computer_choice=random.randomint(0,2)
+user_choice=int(input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors."))
+computer_choice=random.randint(0,2)
+user_choice_converted=None
+computer_choice_converted=None
 
-print(f"Computer chose")
-if user_choice==computer_choice:
-  print("It's a draw")
+if computer_choice==0:
+    computer_choice_converted="Rock"
+    print(rock)
+elif computer_choice==1:
+    computer_choice_converted="Paper"
+    print(paper)
+else:
+    computer_choice_converted="Scissors"
+    print(scissors)
+print(f"Computer chose {computer_choice_converted}")     
+
+if user_choice==0:
+    user_choice_converted="Rock"
+    print(rock)
+elif user_choice==1:
+    user_choice_converted="Paper"
+    print(paper)
+elif user_choice==2:
+    user_choice_converted="Scissors"
+    print(scissors)
+print(f"User chose {user_choice_converted}")
+
+
+
+
+if user_choice==0 and computer_choice==1:
+    print("Computer wins")  
+elif user_choice==0 and computer_choice==2:   
+    print("User wins")
+elif user_choice==1 and computer_choice==0:   
+    print("Computer wins")
+elif user_choice==1 and computer_choice==2:   
+    print("Computer wins")
+elif user_choice==2 and computer_choice==0:   
+    print("Computer wins")
+elif user_choice==2 and computer_choice==1:   
+    print("User wins")
+else:
+    print("It's a draw")
+
+   
