d51915b5419b1b1ccb9f6efc1cd92def8c9dd11d
TimeForward <roman.nasyrov@gmail.com>
2022-06-28 11:50:19 +0300

    hurdle 4 solution

--- # day 6 theory.py
+++ # day 6 theory.py
@@ -60,21 +60,42 @@ def my_function():
 # while not at_goal():
 #     jump()
  # hurdle 3 - https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
- # solution   
-  def jump():
+#  # solution   
+#   def jump():
     
-    turn_left()
-    move()
-    turn_right()
-    move()
-    turn_right()
-    move()
-    turn_left()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
 #The functions move() and turn_left().
 #The conditions front_is_clear() or wall_in_front(), at_goal(), and their negation.
 #How to use a while loop and an if statement.
-while not at_goal():
-    if front_is_clear():
+#hurdle 4 solution http://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json:
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    turn_left()
+    while wall_on_right():
         move()
+    
+    turn_right()
+    move()
+    turn_right()    
+    
+    while front_is_clear():
+        move()
+    turn_left()
+    
+while not at_goal():
+    if wall_in_front():
+        jump()
     else:
-        jump()   
+        move()
+          
+    
+    
