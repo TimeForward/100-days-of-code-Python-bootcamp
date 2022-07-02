33e0f10f1ffab74f37884dd5acae7b5c9014d936
TimeForward <roman.nasyrov@gmail.com>
2022-07-02 20:46:48 +0300

    initial commit

---
+++ # day 8 theory.py
@@ -0,0 +1,17 @@
+# day 8 theory 
+# functions with input
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+# def greeting():
+#   print("Hello there")
+#   print("You smell like a winner")
+#   print("You can do it, and I'll be the first one to congratulate you.I am also there all along the way. Jesus loves and supports you.")
+# greeting()
+def greeting_with_name(name):
+  print(f"Hello there {name}")
+  print(f"You smell like a winner {name}")
+  print("You can do it, and I'll be the first one to congratulate you.I am also there all along the way. Jesus loves and supports you.")
+greeting_with_name("Roman")
+
