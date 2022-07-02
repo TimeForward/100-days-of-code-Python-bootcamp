2d4d1cd6f42b7c6eb00795d5ee750a4d145860a1
TimeForward <roman.nasyrov@gmail.com>
2022-07-02 21:26:08 +0300

    subsequent commit

--- # day 8 theory.py
+++ # day 8 theory.py
@@ -9,9 +9,17 @@
 #   print("You smell like a winner")
 #   print("You can do it, and I'll be the first one to congratulate you.I am also there all along the way. Jesus loves and supports you.")
 # greeting()
-def greeting_with_name(name):
-  print(f"Hello there {name}")
-  print(f"You smell like a winner {name}")
-  print("You can do it, and I'll be the first one to congratulate you.I am also there all along the way. Jesus loves and supports you.")
-greeting_with_name("Roman")
-
+# def greeting_with_name(name):
+#   print(f"Hello there {name}")
+#   print(f"You smell like a winner {name}")
+#   print("You can do it, and I'll be the first one to congratulate you.I am also there all along the way. Jesus loves and supports you.")
+# greeting_with_name("Roman")
+# FUNCTIONS with more than 1 paramter:
+# POSITIONAL PARAMETERS
+# def greet_with(name, location):
+#     print (f"Hello, {name}.\n What is it like in {location}?")
+# greet_with("Roman", "Navoi")
+# KEYWORD PARAMETERS
+def greet_with(name, location):
+    print (f"Hello, {name}.\n What is it like in {location}?")
+greet_with(location="Navoi",  name="Roman")
