4c7bfe183679a9a200aab82797fa2f34c183584a
TimeForward <roman.nasyrov@gmail.com>
2022-07-04 16:26:48 +0300

    comments added

--- # day 8 exercise 3 Ceasars cypher my version.py
+++ # day 8 exercise 3 Ceasars cypher my version.py
@@ -9,21 +9,6 @@ shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(text, shift):
-  encrypted_text=[]
-  alphabet_index=0
-  shifted_index=0
-  
-  for i in text:    
-    alphabet_index=alphabet.index(i)
-    shifted_index=alphabet_index+shift
-    if shifted_index>=len(alphabet):
-        shifted_index=shifted_index-len(alphabet)
-    
-    encrypted_text+=alphabet[shifted_index]
-    encrypted_text_to_string = ''.join(encrypted_text)
-  print(encrypted_text_to_string)
-if direction=="encode":
-    encrypt(text, shift)    
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
     #e.g. 
@@ -37,4 +22,25 @@ if direction=="encode":
 
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
+  encrypted_text=[]
+  alphabet_index=0
+  shifted_index=0
+  
+  for i in text:    
+    #getting letter index 
+    alphabet_index=alphabet.index(i)
+    #and adding the shift to it
+    shifted_index=alphabet_index+shift
+    #adjusting for when shifted_index is beyond indexable range
+    if shifted_index>=len(alphabet):
+        shifted_index=shifted_index-len(alphabet)
+    # getting letters with shifted index 
+    encrypted_text+=alphabet[shifted_index]
+    #list to string conversion
+    encrypted_text_to_string = ''.join(encrypted_text)
+  print(encrypted_text_to_string)
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+# if "encode" was chosen: call "encrypt" function
+if direction=="encode":
+    encrypt(text, shift)    
+
