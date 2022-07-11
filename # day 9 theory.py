2e1997d2ffaea5d2f63e76a719406d12181685b4
TimeForward <roman.nasyrov@gmail.com>
2022-07-11 21:11:16 +0300

    day 9 ex 1

---
+++ # day 9 theory.py
@@ -0,0 +1,16 @@
+# day 9 theory
+#dictionaries
+#retrieving:
+prog_dictionary={
+"Bug":"A computer code error",
+"Freeze":"To stop respoinding (of a program)"
+}
+# print(prog_dictionary["Freeze"])
+# adding an item to dictionary:
+prog_dictionary["Loop"] = "The action of doing something over and over"
+print(prog_dictionary)
+# wiping/creating a new  a dictionary:
+#prog_dictionary={}
+# EDITING:
+prog_dictionary["Bug"]="A moth in your keyboard"
+print(prog_dictionary)
